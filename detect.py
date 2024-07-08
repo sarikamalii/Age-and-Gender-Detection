d41f8728e982b581d754aed8b486e469e49c96ec
sarikamalii <168703644+sarikamalii@users.noreply.github.com>
2024-07-08 08:48:27 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -42,7 +42,7 @@ genderModel="gender_net.caffemodel"
 
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList=['Female','Male']
+genderList=['Male','Female']
 
 #load network
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
